d742c6fa38ff5eb52628dcc245acfc61f65e7a50
kuter <contact@devktr.pl>
2019-12-09 22:39:19 +0100

    added index template

--- polls/main.py
+++ polls/main.py
@@ -1,15 +1,21 @@
 import sys
 
 from aiohttp import web
+import aiohttp_jinja2
+import jinja2
 
 from .db import close_pg, init_pg
 from .routes import setup_routes
-from .settings import config
+from .settings import BASE_DIR, config
 
 
 async def init_app(argv=None):
     app = web.Application()
     app["config"] = config
+    aiohttp_jinja2.setup(
+        app,
+        loader=jinja2.FileSystemLoader(str(BASE_DIR / "polls" / "templates")),
+    )
     setup_routes(app)
     app.on_startup.append(init_pg)
     app.on_cleanup.append(close_pg)
--- polls/views.py
+++ polls/views.py
@@ -1,11 +1,12 @@
-from aiohttp import web
+import aiohttp_jinja2
 
 from .db import question
 
 
+@aiohttp_jinja2.template("index.html")
 async def index(request):
     async with request.app["db"].acquire() as conn:
         cursor = await conn.execute(question.select())
         records = await cursor.fetchall()
         questions = [dict(q) for q in records]
-        return web.Response(text=str(questions))
+        return {"questions": questions}
